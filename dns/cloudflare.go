fc8f59bbff9cf4ad5639a006078634ea0085f963
Separatee <162851813+Separatee@users.noreply.github.com>
2024-06-14 01:44:50 +0800

    Update cloudflare.go

--- dns/cloudflare.go
+++ dns/cloudflare.go
@@ -1,254 +1,215 @@
 package dns
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"strconv"
-	"strings"
+    "fmt"
+    "net/http"
+    "strings"
+    "time"
 
-	"github.com/jeessy2/ddns-go/v6/config"
-	"github.com/jeessy2/ddns-go/v6/util"
+    "github.com/jeessy2/ddns-go/config"
+    "github.com/jeessy2/ddns-go/util"
 )
 
 const (
-	zonesAPI string = "https://api.cloudflare.com/client/v4/zones"
+    zonesAPI = "https://api.cloudflare.com/client/v4/zones"
 )
 
-// Cloudflare Cloudflare实现
 type Cloudflare struct {
-	DNS     config.DNS
-	Domains config.Domains
-	TTL     int
+    DNSConfig
+    Domains config.Domains
 }
 
-// CloudflareZonesResp cloudflare zones返回结果
-type CloudflareZonesResp struct {
-	CloudflareStatus
-	Result []struct {
-		ID     string
-		Name   string
-		Status string
-		Paused bool
-	}
+type CloudflareResponse struct {
+    Success  bool                   `json:"success"`
+    Messages []string               `json:"messages"`
+    Errors   []CloudflareError      `json:"errors"`
+    Result   []CloudflareZoneResult `json:"result"`
 }
 
-// CloudflareRecordsResp records
 type CloudflareRecordsResp struct {
-	CloudflareStatus
-	Result []CloudflareRecord
+    Success  bool                     `json:"success"`
+    Messages []string                 `json:"messages"`
+    Errors   []CloudflareError        `json:"errors"`
+    Result   []CloudflareRecordResult `json:"result"`
 }
 
-// CloudflareRecord 记录实体
-type CloudflareRecord struct {
-	ID      string `json:"id"`
-	Name    string `json:"name"`
-	Type    string `json:"type"`
-	Content string `json:"content"`
-	Proxied bool   `json:"proxied"`
-	TTL     int    `json:"ttl"`
-	Comment string `json:"comment"`
+type CloudflareError struct {
+    Code    int    `json:"code"`
+    Message string `json:"message"`
 }
 
-// CloudflareStatus 公共状态
-type CloudflareStatus struct {
-	Success  bool
-	Messages []string
+type CloudflareZoneResult struct {
+    ID   string `json:"id"`
+    Name string `json:"name"`
 }
 
-// Init 初始化
-func (cf *Cloudflare) Init(dnsConf *config.DnsConfig, ipv4cache *util.IpCache, ipv6cache *util.IpCache) {
-	cf.Domains.Ipv4Cache = ipv4cache
-	cf.Domains.Ipv6Cache = ipv6cache
-	cf.DNS = dnsConf.DNS
-	cf.Domains.GetNewIp(dnsConf)
-	if dnsConf.TTL == "" {
-		// 默认1 auto ttl
-		cf.TTL = 1
-	} else {
-		ttl, err := strconv.Atoi(dnsConf.TTL)
-		if err != nil {
-			cf.TTL = 1
-		} else {
-			cf.TTL = ttl
-		}
-	}
+type CloudflareRecordResult struct {
+    ID        string `json:"id"`
+    Type      string `json:"type"`
+    Name      string `json:"name"`
+    Content   string `json:"content"`
+    Proxied   bool   `json:"proxied"`
+    CreatedOn string `json:"created_on"`
+    ModifiedOn string `json:"modified_on"`
 }
 
-// AddUpdateDomainRecords 添加或更新IPv4/IPv6记录
+func NewCloudflare(dnsConfig DNSConfig) *Cloudflare {
+    return &Cloudflare{DNSConfig: dnsConfig}
+}
+
 func (cf *Cloudflare) AddUpdateDomainRecords() config.Domains {
-	cf.addUpdateDomainRecords("A")
-	cf.addUpdateDomainRecords("AAAA")
-	return cf.Domains
+    cf.addUpdateDomainRecords("A")
+    cf.addUpdateDomainRecords("AAAA")
+    return cf.Domains
 }
 
 func (cf *Cloudflare) addUpdateDomainRecords(recordType string) {
-	ipAddr, domains := cf.Domains.GetNewIpResult(recordType)
+    ipAddr, domains := cf.Domains.GetNewIpResult(recordType)
+    if ipAddr == "" {
+        return
+    }
 
-	if ipAddr == "" {
-		return
-	}
+    for _, domain := range domains {
+        // get zone
+        result, err := cf.getZones(domain)
+        if err != nil {
+            util.Log("查询域名信息发生异常! %s", err)
+            domain.UpdateStatus = config.UpdatedFailed
+            continue
+        }
+        if len(result.Result) == 0 {
+            util.Log("在DNS服务商中未找到根域名: %s", domain.DomainName)
+            domain.UpdateStatus = config.UpdatedFailed
+            continue
+        }
 
-	for _, domain := range domains {
-		// get zone
-		result, err := cf.getZones(domain)
+        zoneID := result.Result[0].ID
+        var records CloudflareRecordsResp
+        // 获取现有记录
+        err = cf.request(
+            "GET",
+            fmt.Sprintf(zonesAPI+"/%s/dns_records?type=%s&name=%s&per_page=50", zoneID, recordType, domain.GetSubDomain()+"."+domain.GetTopDomain()),
+            nil, &records,
+        )
+        if err != nil {
+            util.Log("查询域名信息发生异常! %s", err)
+            domain.UpdateStatus = config.UpdatedFailed
+            continue
+        }
+        if !records.Success {
+            util.Log("查询域名信息发生异常! %s", strings.Join(records.Messages, ", "))
+            domain.UpdateStatus = config.UpdatedFailed
+            continue
+        }
 
-		if err != nil {
-			util.Log("查询域名信息发生异常! %s", err)
-			domain.UpdateStatus = config.UpdatedFailed
-			return
-		}
+        // 根据记录存在与否决定添加或更新
+        if len(records.Result) > 0 {
+            cf.modify(records, zoneID, domain, ipAddr)
+        } else {
+            cf.create(zoneID, domain, recordType, ipAddr)
+        }
 
-		if len(result.Result) == 0 {
-			util.Log("在DNS服务商中未找到根域名: %s", domain.DomainName)
-			domain.UpdateStatus = config.UpdatedFailed
-			return
-		}
-
-		// 存在参数才进行筛选
-		comment := domain.GetCustomParams().Get("comment")
-		if comment != "" {
-			comment = fmt.Sprintf("&comment=%s", comment)
-		}
-
-		zoneID := result.Result[0].ID
-
-		var records CloudflareRecordsResp
-		// getDomains 最多更新前50条
-		err = cf.request(
-			"GET",
-			fmt.Sprintf(zonesAPI+"/%s/dns_records?type=%s&name=%s&per_page=50%s", zoneID, recordType, domain, comment),
-			nil,
-			&records,
-		)
-
-		if err != nil {
-			util.Log("查询域名信息发生异常! %s", err)
-			domain.UpdateStatus = config.UpdatedFailed
-			return
-		}
-
-		if !records.Success {
-			util.Log("查询域名信息发生异常! %s", strings.Join(records.Messages, ", "))
-			domain.UpdateStatus = config.UpdatedFailed
-			return
-		}
-
-		if len(records.Result) > 0 {
-			// 更新
-			cf.modify(records, zoneID, domain, ipAddr)
-		} else {
-			// 新增
-			cf.create(zoneID, domain, recordType, ipAddr)
-		}
-	}
+        // 清理多余的相同解析记录
+        cf.cleanDuplicateRecords(zoneID, recordType, domain, records)
+    }
 }
 
-// 创建
-func (cf *Cloudflare) create(zoneID string, domain *config.Domain, recordType string, ipAddr string) {
-	record := &CloudflareRecord{
-		Type:    recordType,
-		Name:    domain.String(),
-		Content: ipAddr,
-		Proxied: false,
-		TTL:     cf.TTL,
-		Comment: domain.GetCustomParams().Get("comment"),
-	}
-	record.Proxied = domain.GetCustomParams().Get("proxied") == "true"
-	var status CloudflareStatus
-	err := cf.request(
-		"POST",
-		fmt.Sprintf(zonesAPI+"/%s/dns_records", zoneID),
-		record,
-		&status,
-	)
-
-	if err != nil {
-		util.Log("新增域名解析 %s 失败! 异常信息: %s", domain, err)
-		domain.UpdateStatus = config.UpdatedFailed
-		return
-	}
-
-	if status.Success {
-		util.Log("新增域名解析 %s 成功! IP: %s", domain, ipAddr)
-		domain.UpdateStatus = config.UpdatedSuccess
-	} else {
-		util.Log("新增域名解析 %s 失败! 异常信息: %s", domain, strings.Join(status.Messages, ", "))
-		domain.UpdateStatus = config.UpdatedFailed
-	}
+func (cf *Cloudflare) getZones(domain config.Domain) (*CloudflareResponse, error) {
+    var result CloudflareResponse
+    err := cf.request("GET", zonesAPI+"?name="+domain.GetTopDomain(), nil, &result)
+    return &result, err
 }
 
-// 修改
-func (cf *Cloudflare) modify(result CloudflareRecordsResp, zoneID string, domain *config.Domain, ipAddr string) {
-	for _, record := range result.Result {
-		// 相同不修改
-		if record.Content == ipAddr {
-			util.Log("你的IP %s 没有变化, 域名 %s", ipAddr, domain)
-			continue
-		}
-		var status CloudflareStatus
-		record.Content = ipAddr
-		record.TTL = cf.TTL
-		// 存在参数才修改proxied
-		if domain.GetCustomParams().Has("proxied") {
-			record.Proxied = domain.GetCustomParams().Get("proxied") == "true"
-		}
-		err := cf.request(
-			"PUT",
-			fmt.Sprintf(zonesAPI+"/%s/dns_records/%s", zoneID, record.ID),
-			record,
-			&status,
-		)
+func (cf *Cloudflare) create(zoneID string, domain config.Domain, recordType, ipAddr string) {
+    record := map[string]interface{}{
+        "type":    recordType,
+        "name":    domain.GetSubDomain(),
+        "content": ipAddr,
+        "ttl":     cf.TTL,
+        "proxied": cf.Proxy,
+    }
 
-		if err != nil {
-			util.Log("更新域名解析 %s 失败! 异常信息: %s", domain, err)
-			domain.UpdateStatus = config.UpdatedFailed
-			return
-		}
-
-		if status.Success {
-			util.Log("更新域名解析 %s 成功! IP: %s", domain, ipAddr)
-			domain.UpdateStatus = config.UpdatedSuccess
-		} else {
-			util.Log("更新域名解析 %s 失败! 异常信息: %s", domain, strings.Join(status.Messages, ", "))
-			domain.UpdateStatus = config.UpdatedFailed
-		}
-	}
+    var result CloudflareResponse
+    err := cf.request("POST", fmt.Sprintf(zonesAPI+"/%s/dns_records", zoneID), record, &result)
+    if err != nil || !result.Success {
+        util.Log("添加DNS记录失败! %s", strings.Join(result.Messages, ", "))
+        domain.UpdateStatus = config.UpdatedFailed
+    } else {
+        util.Log("添加DNS记录成功!")
+        domain.UpdateStatus = config.UpdatedSuccess
+    }
 }
 
-// 获得域名记录列表
-func (cf *Cloudflare) getZones(domain *config.Domain) (result CloudflareZonesResp, err error) {
-	err = cf.request(
-		"GET",
-		fmt.Sprintf(zonesAPI+"?name=%s&status=%s&per_page=%s", domain.DomainName, "active", "50"),
-		nil,
-		&result,
-	)
+func (cf *Cloudflare) modify(records CloudflareRecordsResp, zoneID string, domain config.Domain, ipAddr string) {
+    record := map[string]interface{}{
+        "type":    records.Result[0].Type,
+        "name":    records.Result[0].Name,
+        "content": ipAddr,
+        "ttl":     cf.TTL,
+        "proxied": records.Result[0].Proxied,
+    }
 
-	return
+    var result CloudflareResponse
+    err := cf.request("PUT", fmt.Sprintf(zonesAPI+"/%s/dns_records/%s", zoneID, records.Result[0].ID), record, &result)
+    if err != nil || !result.Success {
+        util.Log("更新DNS记录失败! %s", strings.Join(result.Messages, ", "))
+        domain.UpdateStatus = config.UpdatedFailed
+    } else {
+        util.Log("更新DNS记录成功!")
+        domain.UpdateStatus = config.UpdatedSuccess
+    }
 }
 
-// request 统一请求接口
-func (cf *Cloudflare) request(method string, url string, data interface{}, result interface{}) (err error) {
-	jsonStr := make([]byte, 0)
-	if data != nil {
-		jsonStr, _ = json.Marshal(data)
-	}
-	req, err := http.NewRequest(
-		method,
-		url,
-		bytes.NewBuffer(jsonStr),
-	)
-	if err != nil {
-		return
-	}
-	req.Header.Set("Authorization", "Bearer "+cf.DNS.Secret)
-	req.Header.Set("Content-Type", "application/json")
+func (cf *Cloudflare) cleanDuplicateRecords(zoneID, recordType string, domain config.Domain, records CloudflareRecordsResp) {
+    // 获取最新的解析记录ID
+    var latestRecordID string
+    latestTime := time.Time{}
+    for _, record := range records.Result {
+        // 比较解析记录的创建时间或修改时间，找到最新的记录
+        recordTime, err := time.Parse(time.RFC3339, record.CreatedOn)
+        if err != nil {
+            recordTime, err = time.Parse(time.RFC3339, record.ModifiedOn)
+            if err != nil {
+                continue
+            }
+        }
+        if recordTime.After(latestTime) {
+            latestTime = recordTime
+            latestRecordID = record.ID
+        }
+    }
 
-	client := util.CreateHTTPClient()
-	resp, err := client.Do(req)
-	err = util.GetHTTPResponse(resp, err, result)
+    // 删除多余的相同解析记录
+    for _, record := range records.Result {
+        if record.ID != latestRecordID {
+            var result CloudflareResponse
+            err := cf.request("DELETE", fmt.Sprintf(zonesAPI+"/%s/dns_records/%s", zoneID, record.ID), nil, &result)
+            if err != nil || !result.Success {
+                util.Log("删除多余DNS记录失败! %s", strings.Join(result.Messages, ", "))
+            } else {
+                util.Log("删除多余DNS记录成功!")
+            }
+        }
+    }
+}
 
-	return
+func (cf *Cloudflare) request(method, url string, body interface{}, result interface{}) error {
+    client := &http.Client{
+        Timeout: time.Second * 30,
+    }
+    req, err := util.NewJSONRequest(method, url, body)
+    if err != nil {
+        return err
+    }
+
+    req.Header.Set("Authorization", "Bearer "+cf.DNSConfig.Secret)
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := client.Do(req)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    return util.ParseJSONResponse(resp.Body, result)
 }
